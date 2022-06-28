f6dde643db18cfa1703db72a17e6ce872f9a5c94
Jens Krumsieck <jenso666@live.de>
2022-06-28 21:01:43 +0200

    Initial Commit

---
+++ QRCodeGen/assets/index.ts
@@ -0,0 +1,10 @@
+﻿ import "@fontsource/source-sans-pro/200.css";
+  import "@fontsource/source-sans-pro/200-italic.css";
+  import "@fontsource/source-sans-pro/400.css";
+  import "@fontsource/source-sans-pro/400-italic.css";
+  import "@fontsource/source-sans-pro/600.css";
+  import "@fontsource/source-sans-pro/600-italic.css";
+  import "@fontsource/source-sans-pro/700.css";
+  import "@fontsource/source-sans-pro/700-italic.css";
+  import "@fontsource/source-sans-pro/900.css";
+  import "@fontsource/source-sans-pro/900-italic.css";
---
+++ QRCodeGen/assets/tailwind.config.js
@@ -0,0 +1,32 @@
+﻿module.exports = {
+    content: [
+        '../**/*.{razor,html}',
+        '../wwwroot/index.html'
+    ],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            green: '#9CD919',
+            'dark-green': '#52A62D',
+            'light-green': '#B7D969',
+            dark: '#333333',
+            white: '#FFFFFF',
+            black: '#000000',
+            gray: '#F1F1F1',
+            red: '#DA0037'
+        },
+        fontFamily: {
+            sans: ['Source Sans Pro', 'sans-serif']
+        },
+        extend: {
+            letterSpacing: {
+                tightest: '-.065em'
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms')
+    ],
+}
